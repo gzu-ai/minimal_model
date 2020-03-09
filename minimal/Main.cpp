ed9c67c1588e5d1dbd9ccd8936448bd3d4434994
xie_zhongtao <xie_zhongtao@163.com>
2020-03-09 18:11:02 +0800

    clear test code

--- minimal/Main.cpp
+++ minimal/Main.cpp
@@ -33,22 +33,6 @@ static void SIGINT_exit(int) {
 int main(int argc, char** argv)
 {
 
-    int data[] = {1,2,3,4,5,6};
-    int index=0;
-    int lastIndex=0;
-    int size=6;
-    for ( ;lastIndex < size; ++lastIndex) {
-        data[index]=data[lastIndex];
-        if (data[lastIndex]%2==0){
-            continue;
-        }
-        ++index;
-    }
-
-//    data[index]=-1;
-//    printf("sss");
-
-
     try {
         setUsageHelp("USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
         setX86FPUPrecision();
